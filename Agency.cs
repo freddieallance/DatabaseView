48884be64a850c32e9ae636aae3d091708a7aca2
freddieallance <122672447+freddieallance@users.noreply.github.com>
2024-05-21 10:05:55 +0800

    Test

---
+++ Agency.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WpfApp3;
+
+namespace WpfApp3
+{
+    public class Agency
+    {
+        [Key] // This attribute specifies that 'objectid' is the primary key
+        public string objectid { get; set; }
+
+        public string agency_code { get; set; }
+        public string agency_name { get; set; }
+    }
+}
---
+++ DataContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp3
+{
+    public class DataContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            // Replace 'server', 'user', 'password', and 'database' with your MySQL connection details
+            string connectionString = "server=localhost;user=root;password=allance99;database=ela_db;";
+            //string connectionString = "server=172.26.80.150;user=rbs;password=rbsdbadmin;database=Ela_DB;";
+            //string connectionString = "server=localhost;user=root;password=allance99;database=ela_db;";
+
+            optionsBuilder.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString));
+        }
+        public DbSet<Agency> agency { get; set; }
+    }
+}
